2e241da5baf111858c1e8086700e7b84a514183e
nidhishkrishnan <nidhishkrishnan@gmail.com>
2020-05-22 22:41:08 +0200

    Initial Commit

---
+++ src/main/java/com/spring/resilience4j/SpringResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.spring.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResilience4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/resilience4j/SpringResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
