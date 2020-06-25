b0a49790b3e2a96bef5fa79d4f0f58440c2a7800
nidhishkrishnan <nidhishkrishnan@gmail.com>
2020-06-25 23:27:14 +0200

    Adding resilience4j implementation

---
+++ src/main/java/com/spring/resilience4j/service/HelloWorldResilience.java
@@ -0,0 +1,26 @@
+package com.spring.resilience4j.service;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class HelloWorldResilience {
+
+    private final RestTemplate restTemplate;
+
+    @CircuitBreaker(name = "hello-world", fallbackMethod = "fallbackForHelloWorld")
+    public String getHelloWorld() {
+        log.info("calling getHelloWorld()");
+        return restTemplate.getForObject("/world", String.class);
+    }
+
+    public String fallbackForHelloWorld(Throwable t) {
+        log.error("Inside fallbackForGetSeller, cause - {}", t.toString());
+        return "Sorry ... Service not available!!!";
+    }
+}
--- src/main/java/com/spring/resilience4j/service/HelloWorldService.java
+++ src/main/java/com/spring/resilience4j/service/HelloWorldService.java
@@ -1,11 +1,15 @@
 package com.spring.resilience4j.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class HelloWorldService {
 
+    private final HelloWorldResilience helloWorldResilience;
+
     public String getHelloWorld() {
-        return "Hello World";
+        return helloWorldResilience.getHelloWorld();
     }
 }
